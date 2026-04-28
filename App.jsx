73e38353dafbc4f2ac45024d4b7b1aa9d31cb47d
londayork <yoyodeats@gmail.com>
2026-04-28 10:01:27 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -254,7 +254,7 @@ export default function YoYosStore() {
     });
 
     // 🔥 STRIPE CHECKOUT
-    const res = await fetch("/api/checkout", {
+    const res = await fetch("/api/create-checkout-session", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
