bbebd45b61b49f2da6c32b0bcadd0ec58513e7ca
londayork <yoyodeats@gmail.com>
2026-05-05 14:25:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import YoYosStore from "./App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <YoYosStore />
+    <App />
   </React.StrictMode>
 );
